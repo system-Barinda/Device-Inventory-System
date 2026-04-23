a7c387c9b1649a1210e535ff32d9f576ce06881e
system sylvere BARINDA <systembarinda@gmail.com>
2026-04-23 22:04:37 +0200

    initial setup to all file Will need

---
+++ src/main/java/com/airtel/Device_inventory_system/AssetInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.airtel.Device_inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtel/Device_inventory_system/AssetInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtel.Device_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
